d6f14599be1c77c9da915a56e569d483426da8b7
Divyanshu B <divyanshub2311@gmail.com>
2026-04-18 17:17:37 -0500

    feat: swapped Ollama LLM tp Groq, increase context and output limits

--- rag/generator.py
+++ rag/generator.py
@@ -1,25 +1,26 @@
-import requests
 import os
 from dotenv import load_dotenv
+from groq import Groq
+import re
 
 load_dotenv()
 
-OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL")
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+GROQ_MODEL = "qwen/qwen3-32b"
+
 
 def generate(question: str, context_chunks: list) -> str:
     context = "\n\n".join(
         f"[{c['ticker']}] {c['text']}" for c in context_chunks
     )
-    prompt = f"""You are a helpful assistant for answering questions about SEC filings. Use only the following context to answer the question. If you don't know the answer, say you don't know. Always include the ticker symbol in your answer.\n\nContext:\n{context}\n\nQuestion: {question} \n Answer:""" 
+    prompt = f"""You are a helpful assistant for answering questions about SEC filings. Use only the following context to answer the question. Always answer in 2-3 sentences. If you don't know the answer, say you don't know. Always include the ticker symbol in your answer.\n\nContext:\n{context}\n\nQuestion: {question} \n Answer:""" 
 
-    response = requests.post(
-        f"{OLLAMA_BASE_URL}/api/generate",
-        json={
-            "model": OLLAMA_MODEL,
-            "prompt": prompt,
-            "stream": False,
-            "temperature": 0.2,
-        },
+    response = client.chat.completions.create(
+        model=GROQ_MODEL,
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=1024,
+        temperature=0.2,
     )
-    return response.json()["response"]
+    content = response.choices[0].message.content.strip()
+    content = re.sub(r"<think>.*?</think>", "", content, flags=re.DOTALL).strip()
+    return content
